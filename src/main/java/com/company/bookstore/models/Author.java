9dc8e09ccc39ed3ec857fc103c2f3486dba23508
Mauricio Garcia <mauriciogarcia@Mauricios-MacBook-Pro.local>
2023-08-08 14:38:27 -0500

    updated controller and model

--- src/main/java/com/company/bookstore/models/Author.java
+++ src/main/java/com/company/bookstore/models/Author.java
@@ -13,7 +13,14 @@ public class Author implements Serializable {
     @Column(name = "author_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     int id;
-    String name;
+    String first_name;
+    String last_name;
+    String street;
+    String city;
+    String state;
+    String postal_code;
+    String phone;
+    String email;
 
     public int getId() {
         return id;
@@ -23,11 +30,71 @@ public class Author implements Serializable {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getFirst_name() {
+        return first_name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostal_code() {
+        return postal_code;
+    }
+
+    public void setPostal_code(String postal_code) {
+        this.postal_code = postal_code;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return getFirst_name() + " " + getLast_name();
     }
 }
